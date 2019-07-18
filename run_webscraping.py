75347c17570bb1cd2b57a82d6064dd75e188d08b
Rylan Kasitz <rk069310@cerner.net>
2019-07-18 09:12:46 -0500

    setup some more stuff

---
+++ run_webscraping.py
@@ -0,0 +1,3 @@
+import webscrapers.ksu.course_scraper as ksu_course_scraper
+
+ksu_course_scraper.run()
---
+++ webscrapers/ksu/course_scraper.py
@@ -0,0 +1,6 @@
+import lib.webscraper.config as webscraper_config
+
+URL = ''
+
+def run():
+    context = webscraper_config.get_context(URL)
